28c30747abebbecd3ccf02bcbd69b4ba10c694ff
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-09-10 12:29:35 +0500

    :art: Added button

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <Button variant="contained" sx={{width: 300, height: 50, borderRadius: 3}}>
+      <span>+</span>
+      <span>Create New Group</span>
+    </Button>
+  )
+}
+
+export default Button
